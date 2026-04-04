0748759fd34e9e14e0561d6a8a7789bf197b4302
Базарнов Роман <bazarnoffroman@yandex.ru>
2026-04-04 22:31:09 +0300

    test

--- lab2_06_spring_security_jwt/src/main/java/ru/itis/dis403/lab2_06/Lab206SpringSecurityJwtApplication.java
+++ lab2_06_spring_security_jwt/src/main/java/ru/itis/dis403/lab2_06/Lab206SpringSecurityJwtApplication.java
@@ -6,7 +6,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Lab206SpringSecurityJwtApplication {
-
 	public static void main(String[] args) {
 
         SpringApplication.run(Lab206SpringSecurityJwtApplication.class, args);

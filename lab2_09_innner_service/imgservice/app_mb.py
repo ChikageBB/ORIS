078864d46b2ec57513f5e4675ad868dc9b030e05
Базарнов Роман <bazarnoffroman@yandex.ru>
2026-04-26 20:39:55 +0300

    homework

---
+++ lab2_09_innner_service/imgservice/app_mb.py
@@ -0,0 +1,38 @@
+import asyncio
+import json
+from os import name
+
+import nats
+from nats.aio.msg import Msg
+
+async def main():
+    nc = await nats.connect("nats://localhost:4222")
+
+    async def request_handler(msg: Msg):
+        try:
+            print(f"Получен запрос: {msg.data.decode()}")
+            request_data = json.loads(msg.data.decode())
+
+            response = {
+                "status": "success",
+                "received": request_data,
+            }
+            response_json = json.dumps(response)
+
+            await msg.respond(response_json.encode())
+            print("Ответ отправлен")
+        except json.JSONDecodeError as e:
+            error_response = {"status": "error", "message": f"Invalid JSON: {str(e)}"}
+            await msg.respond(json.dumps(error_response).encode())
+
+    await nc.subscribe("request.image.mirror", cb=request_handler)
+    print("Python‑сервер запущен, ожидает запросы на 'request.image.mirror'")
+
+    # Держим сервер активным
+    try:
+        await asyncio.Future()  # бесконечное ожидание
+    except KeyboardInterrupt:
+        await nc.close()
+
+if name == "__main__":
+    asyncio.run(main())
---
+++ lab2_09_innner_service/java-service/src/main/java/ru/itis/dis403/javaservice/service/ImageService.java
@@ -0,0 +1,13 @@
+package ru.itis.dis403.javaservice.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ImageService {
+
+    private List<String> imgList = new ArrayList<>();
+
+}
